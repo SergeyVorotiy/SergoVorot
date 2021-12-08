9105b650eaee10184d76558c1b210647986a28ac
SergeyVorotiy <95651513+SergeyVorotiy@users.noreply.github.com>
2021-12-09 01:23:00 +0300

    Configured webpack for project

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const isdev = process.env.NODE_ENV === "development";
+const isProd = !isdev;
+// подключение плагинов для webpack
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.css', '.png'],
+        alias: {
+            '@src': path.resolve(__dirname, 'src'),
+        }
+    },
+    optimization: {
+        minimize: isProd,
+        splitChunks: {
+            chunks: 'all'
+        },
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.pug",
+            minify: {
+                collapseWhitespace: isProd,
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new HtmlWebpackPugPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+
+        ]
+    }
+}
